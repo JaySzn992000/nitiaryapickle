796307a2c70f83eec6d648885511e9654792afa3
Jay Kumar <jay.k.992000@gmail.com>
2025-07-21 12:43:45 +0530

    Initial commit with updated CORS settings

--- src/Login/Address.js
+++ src/Login/Address.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Address = () => {
+  
   const loadRazorpay = (src) => {
     return new Promise((resolve) => {
       const script = document.createElement("script");
